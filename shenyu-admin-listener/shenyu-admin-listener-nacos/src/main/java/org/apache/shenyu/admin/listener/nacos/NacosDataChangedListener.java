a2e0990ee67a0be962a3d2c49e76587e2fe7692e
hdgaadd <2635646017@qq.com>
2024-02-28 10:19:52 +0800

    test commit
    test commit1,
    test commit2,
    test commit3.

--- shenyu-admin-listener/shenyu-admin-listener-nacos/src/main/java/org/apache/shenyu/admin/listener/nacos/NacosDataChangedListener.java
+++ shenyu-admin-listener/shenyu-admin-listener-nacos/src/main/java/org/apache/shenyu/admin/listener/nacos/NacosDataChangedListener.java
@@ -60,6 +60,7 @@ public class NacosDataChangedListener extends AbstractNodeDataChangedListener {
     @Override
     public void doDelConfig(final String dataId) {
 
+
         try {
             configService.removeConfig(
                     dataId,
